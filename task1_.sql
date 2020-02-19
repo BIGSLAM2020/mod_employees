2019042fdc52225debf2df1e78e7ae0a40675a58
sammy <sammyabdulkader@gmail.com>
2020-02-19 00:56:32 -0500

    Task with SQL and CSV exports

---
+++ task1_.sql
@@ -0,0 +1,23 @@
+-- SELECTING INFORMATION FOR t_dept_emp
+SELECT
+	emp_no, from_date, to_date
+FROM
+	t_dept_emp; 
+
+-- SELECTING DISTINCT INFORMATION FOR t_dept_emp
+SELECT DISTINCT
+	emp_no, from_date, to_date
+FROM
+	t_dept_emp;
+    
+# ADDED FOR TABLEAU
+SELECT 
+    YEAR(d.from_date) AS calendar_year,
+    e.gender,
+    COUNT(e.emp_no) AS num_of_employees
+FROM
+    t_employees e
+        JOIN
+    t_dept_emp d ON d.emp_no = e.emp_no
+GROUP BY calendar_year , e.gender
+HAVING calendar_year >= 1990;
